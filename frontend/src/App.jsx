bc415f59f4b8d3032b5120e025b94ddb47a8f4aa
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-20 16:28:41 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
+import AppRouter from "./AppRouter.jsx";
+
 export default function App() {
-  return (
-    <div style={{ padding: 40, fontSize: 24 }}>
-      <h1>G.I. Frontend</h1>
-      <p>Creator Console Online.</p>
-    </div>
-  );
+  return <AppRouter />;
 }
