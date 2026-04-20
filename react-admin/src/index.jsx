efbcf3c4a3535f92322c9add247e5ada40225098
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-20 14:12:57 -0700

    Update index.jsx

--- react-admin/src/index.jsx
+++ react-admin/src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles/admin.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
