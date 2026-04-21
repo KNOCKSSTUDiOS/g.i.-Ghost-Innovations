71cc1449a7c1962d454ba3f848844e2543079701
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 16:46:05 -0700

    Create index.ts

---
+++ gi-core/index.ts
@@ -0,0 +1,10 @@
+export * from "./core";
+export * from "./modules/config";
+export * from "./modules/logger";
+export * from "./modules/storage";
+export * from "./modules/access";
+export * from "./modules/roles";
+export * from "./modules/permissions";
+export * from "./modules/users";
+export * from "./modules/events";
+
