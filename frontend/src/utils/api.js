54b719307b4ce4fc9281725691d59d262b09da71
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 08:50:27 -0700

    Create api.js

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,18 @@
+const API_BASE = "http://localhost:4000";
+
+export async function apiGet(path) {
+  const res = await fetch(API_BASE + path);
+  if (!res.ok) throw new Error("GET request failed");
+  return res.json();
+}
+
+export async function apiPost(path, body = {}) {
+  const res = await fetch(API_BASE + path, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body)
+  });
+  if (!res.ok) throw new Error("POST request failed");
+  return res.json();
+}
+
