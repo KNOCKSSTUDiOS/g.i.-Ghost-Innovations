22f28e848922f6626a415f99638bd1b3591f11ad
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 10:55:40 -0700

    Create log.js

---
+++ backend/src/utils/log.js
@@ -0,0 +1,16 @@
+export function log(...args) {
+  console.log(...args);
+}
+
+export function info(...args) {
+  console.info(...args);
+}
+
+export function warn(...args) {
+  console.warn(...args);
+}
+
+export function error(...args) {
+  console.error(...args);
+}
+
