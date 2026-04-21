cf2fe27635d48294e83024ca558c05a7820b728a
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 09:07:20 -0700

    Update logger.js

--- backend/src/middleware/logger.js
+++ backend/src/middleware/logger.js
@@ -1,5 +1,5 @@
-export const logger = (req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+export function logger(req, res, next) {
+  const time = new Date().toISOString();
+  console.log(`[${time}] ${req.method} ${req.url}`);
   next();
-};
-
+}
