baa56f3fe5aa7465f85c4a6636589c31e57498dd
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 17:46:00 -0700

    Refactor server initialization and logging
    Removed old server setup and extensions registration.

--- server.ts
+++ server.ts
@@ -1,22 +1,3 @@
-import http from "http";
-import { createCore } from "./gi-core/core";
-import { createRouter } from "./gi-router/router";
+import "./gi-entry/start";
 
-import { registerAuthExtension } from "./gi-extensions-auth/auth-extension";
-import { registerUserExtension } from "./gi-extensions-user/user-extension";
-import { registerSystemExtension } from "./gi-extensions-system/system-extension";
-
-const core = createCore();
-const router = createRouter(core);
-
-registerAuthExtension(router, core);
-registerUserExtension(router, core);
-registerSystemExtension(router, core);
-
-const server = http.createServer(async (req, res) => {
-  await router.handle(req, res);
-});
-
-server.listen(8080, () => {
-  core.logger.log("server_started");
-});
+console.log("server_started");
