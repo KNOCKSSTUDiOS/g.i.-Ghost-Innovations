d76eaadfc0d9ad99f8ea9f5a8a8d4a8e8fc0b9b1
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 17:57:05 -0700

    Create engine.index.ts to export engine modules

---
+++ engine.index.ts
@@ -0,0 +1,11 @@
+export * from "./engine.config";
+export * from "./engine.modes";
+export * from "./engine.flags";
+export * from "./engine.state";
+export * from "./engine.context";
+export * from "./engine.lifecycle";
+export * from "./engine.hooks";
+export * from "./engine.loader";
+export * from "./engine.instance";
+export * from "./engine.root";
+
