934bb53b0c4541db24a3e4252e42e48e7aebd65a
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 18:43:34 -0700

    Update stack.ts

--- gi-core/middleware/stack.ts
+++ gi-core/middleware/stack.ts
@@ -1,26 +1,2 @@
-import { Middleware } from "./types";
-import { HttpContext } from "../http/context";
-
-export class MiddlewareStack {
-  private stack: Middleware[] = [];
-
-  use(mw: Middleware) {
-    this.stack.push(mw);
-  }
-
-  async run(ctx: HttpContext, finalHandler: () => Promise<void>) {
-    let index = -1;
-
-    const dispatch = async (i: number): Promise<void> => {
-      if (i <= index) return;
-      index = i;
-
-      const fn = this.stack[i] || finalHandler;
-      if (!fn) return;
-
-      await fn(ctx, () => dispatch(i + 1));
-    };
-
-    await dispatch(0);
-  }
-}
+export * from "./types";
+export * from "./stack";
